78484f6ff2df40bcba4a9d7f123686544ee22366
Alexandre Kerello <akerello@gmail.com>
2025-08-15 00:07:57 +0200

    feat: add a config example file

---
+++ config.example.sh
@@ -0,0 +1,27 @@
+# === MiniMon configuration file ===
+# Email alert via SMTP (optional)
+ENABLE_EMAIL=false
+SMTP_SERVER="smtp.example.com"
+SMTP_PORT=587
+SMTP_TLS=true           # true|false
+SMTP_USER=""
+SMTP_PASS=""
+EMAIL_FROM="minimon@localhost"
+EMAIL_TO=""
+
+# Telegram alert (optional)
+ENABLE_TELEGRAM=false
+TELEGRAM_TOKEN=""
+TELEGRAM_CHAT_ID=""
+
+# Alerts threshold (%)
+CPU_ALERT=85            # % CPU used
+RAM_ALERT=90            # % RAM used
+DISK_ALERT=90           # % disk used on DISK_PATH
+DISK_PATH="/"          # mount point to watch
+
+# Services to monitor (systemd)
+SERVICES=("ssh" "cron")
+
+# Miscellaneous
+LOG_FILE="/var/log/minimon.log"   # usefull if using cron
