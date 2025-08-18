2fa5740a6a9fbce15dcfcf35df498f52cea5fe21
Alexandre-Kerello <akerello@gmail.com>
2025-08-18 21:54:56 +0200

    fix: unexpected EOF error

--- uninstall.sh
+++ uninstall.sh
@@ -5,37 +5,38 @@ INSTALL_DIR="/opt/minimon"
 BIN_LINK="/usr/local/bin/minimon"
 CONFIG_DIR="/etc/minimon"
 
-say() { echo -e "\033[1;32m[MiniMon]\033[0m $*" }
+say() { echo -e "\033[1;32m[MiniMon]\033[0m $*"; }
 
-read -rp "Would you also delete the configuration in $CONFIG_DIR/config.sh ? [y/N]" rm_cfg
+read -rp "Would you also delete the configuration in $CONFIG_DIR/config.sh ? [y/N] " rm_cfg
 
 # Delete symbolic link
-if [ -L "/usr/local/bin/minimon" ]; then
+if [ -L "$BIN_LINK" ]; then
   sudo rm -f "$BIN_LINK"
-  say "Symbolic link /usr/local/bin/minimon deleted"
+  say "Symbolic link $BIN_LINK deleted"
 fi
 
 # Delete installation directory
 if [ -d "$INSTALL_DIR" ]; then
   sudo rm -rf "$INSTALL_DIR"
-  say "Installation directory deleted"
+  say "Installation directory $INSTALL_DIR deleted"
 fi
 
 # Delete configuration
-if [[ "$rm_cfg" =~ ^([[yY][eE][sS]|[yY])$ ]]; then
+if [[ "$rm_cfg" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   sudo rm -rf "$CONFIG_DIR"
-  say "Configuration /etc/minimon deleted"
+  say "Configuration $CONFIG_DIR deleted"
 fi
 
 # Delete minimon permission group
 if getent group minimon >/dev/null; then
-  if [ "$(grep -c "minimon" /etc/group)" -gt 0]; then
-    sudo groupdel minimon || true
-    say "Minimon group deleted"
-  fi
+  sudo groupdel minimon || true
+  say "Minimon group deleted"
 fi
 
 # Delete cron job
-crontab -l 2>/dev/null | grep -v "minimon" | crontab - || true
+if crontab -l 2>/dev/null | grep -q "minimon"; then
+  crontab -l | grep -v "minimon" | crontab -
+  say "MiniMon cron job removed"
+fi
 
 say "MiniMon successfully uninstalled"
