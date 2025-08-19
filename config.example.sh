cdc359be15a4be294bb8e688b146874dc522aaf2
Alexandre-Kerello <akerello@gmail.com>
2025-08-19 23:05:10 +0200

    add comment to config

--- config.example.sh
+++ config.example.sh
@@ -25,4 +25,4 @@ SERVICES=("ssh" "cron")
 
 # Miscellaneous
 LOG_FILE="/var/log/minimon.log"   # usefull if using cron
-REPORT_DIR="/home/$USER"
+REPORT_DIR="/home/$USER"          # don't forget to create the directory
